bc26cb8be2fbf1c5c20485ea4d90c4a3982a171e
KimonasXXX482 <156684112+KimonasXXX482@users.noreply.github.com>
2025-07-04 09:59:01 +0300

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Liberte-Maisonettes/",  // ✅ Add this line
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
